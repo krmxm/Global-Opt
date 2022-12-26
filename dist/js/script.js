1865c3c87bf30dbc86c6a7bdb8cda13a6da16b59
Kramarenko Maxim <kramarenko-90@inbox.ru>
2022-12-27 00:03:24 +0300

    reviews

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: 'auto',
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/prevarrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/slider/nextarrow.png"></img></button>',
     responsive: [
       {
         breakpoint: 768,
