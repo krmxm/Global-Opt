1bfa6935e74f31c2d4647bafe3edd7619c7d6a77
Kramarenko Maxim <kramarenko-90@inbox.ru>
2022-12-26 18:56:33 +0300

    slide

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
