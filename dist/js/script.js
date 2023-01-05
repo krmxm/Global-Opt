d6f8ed999ec43bbbd0f027f710e4cfe177374866
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-01-06 01:53:08 +0300

    carousel media

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-next"><img src="img/slider/nextarrow.png"></img></button>',
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1200,
         settings: {
-          arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          dots: true,
+          slidesToShow: 1
         }
       },
       {
